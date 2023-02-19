3fd551ad34b21666b1fa74209ad3097750be9d6f
Matthew Manning <45296246+mattlovestech@users.noreply.github.com>
2023-02-19 21:07:06 +0000

    added

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import Button from '../components/Button'
 import styles from '../styles/home.module.css'
 
@@ -9,41 +9,39 @@ function throwError() {
   )
 }
 
-function Home() {
+
+function ImageConverter() {
+  const [file, setFile] = useState(null);
+  const [base64Image, setBase64Image] = useState('');
+
+  const handleImageChange = (e) => {
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
+
+    const reader = new FileReader();
+    reader.readAsDataURL(selectedFile);
+
+    reader.onload = () => {
+      setBase64Image(reader.result);
+    };
+  };
 
   return (
-    <main className={styles.main}>
-      <div id="title" style={{textAlign: "center;"}}>
-        <h1>File Converter</h1>
-      </div>
-      <p style ={{color: "blue;", textAlign: "center;"}}>
-        Please input the file you'd like to convert.
-      </p>
-      <div style={{textAlign: "center;"}}>
-        <div style={{textAlign: "center;"}}>
-          <form>
-            {/* <label for="myfile">Convert</label> */}
-            <input onInput={handleFileChange()} style ={{color: "blue;"}} type="file" id="myFile" name="filename"></input>
-            {/* <input style ={{color: "blue;"}} type="submit"></input> */}
-            {/* input.addEventListener('change', handleFileChange); */}
-          </form>
-        </div>
-        <div style={{textAlign: "center;", margin: "10px;" }}>
-          <form action="/send-data-here" method="post">
-              <label style ={{color: "blue;"}} for="target-file-extension">Enter file extension to convert to </label>
-              <input type="text" id="target-file-extension" name="file-ext" />
-              <button style = {{left: "22çpx;"}}type="submit">Submit</button>
-          </form>
-        </div>
-      </div>
-    </main>
-  )
+    <div>
+      <input type="file" onChange={handleImageChange} />
+      {base64Image && (
+        <img src={base64Image} alt="Converted Image" />
+      )}
+    </div>
+  );
 }
 
-export default Home
-
-function handleFileChange(value) {
-  
-  console.log(value);
-
+export default function Home() {
+  return (
+    <div>
+      <h1>Image Converter</h1>
+      <ImageConverter />
+    </div>
+  );
 }
+
